34e593835da78209e7f9cf370a37a7745271c71f
Bringo 2002 <brianashoka@gmail.com>
2025-10-01 15:43:57 +0300

    edited supabaseClient

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
-
- const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
- const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+import {VITE_SUPABASE_URL, VITE_SUPABASE_ANON_KEY} from '../APIdomains'
+ const supabaseUrl = VITE_SUPABASE_URL;
+ const supabaseAnonKey =VITE_SUPABASE_ANON_KEY;
  
  if (!supabaseUrl || !supabaseAnonKey) {
   console.error("Missing Supabase environment variables. Check VITE_SUPABASE_URL and VITE_SUPABASE_ANON_KEY.");
