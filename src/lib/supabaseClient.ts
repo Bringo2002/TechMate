ed54b959c2c5e4895236e7b473c58c14ce1919a3
Bringo 2002 <brianashoka@gmail.com>
2026-02-13 11:03:34 +0300

    login issue solved v1

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,12 +1,18 @@
 import { createClient } from '@supabase/supabase-js';
-import {VITE_SUPABASE_URL, VITE_SUPABASE_ANON_KEY} from '../APIdomains'
- const supabaseUrl = VITE_SUPABASE_URL;
- const supabaseAnonKey =VITE_SUPABASE_ANON_KEY;
- 
- if (!supabaseUrl || !supabaseAnonKey) {
-  console.error("Missing Supabase environment variables. Check VITE_SUPABASE_URL and VITE_SUPABASE_ANON_KEY.");
- }
+import { VITE_SUPABASE_URL, VITE_SUPABASE_ANON_KEY } from '../APIdomains'
+const supabaseUrl = VITE_SUPABASE_URL;
+const supabaseAnonKey = VITE_SUPABASE_ANON_KEY;
 
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
- 
+if (!supabaseUrl || !supabaseAnonKey) {
+    console.error("Missing Supabase environment variables. Check VITE_SUPABASE_URL and VITE_SUPABASE_ANON_KEY.");
+}
+
+const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+    auth: {
+        persistSession: true,
+        autoRefreshToken: true,
+        detectSessionInUrl: true
+    }
+});
+
 export default supabase;
